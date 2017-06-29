daa219931e48b6ed48e64a7abb55d47b37e562ed
Steve Losk <Stevedlosk@gmail.com>
2017-06-28 19:39:07 -0700

    Initial commit

---
+++ app/src/main/java/com/weebly/stevelosk/uv_buddy/ApiTestActivity.java
@@ -0,0 +1,13 @@
+package com.weebly.stevelosk.uv_buddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApiTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_api_test);
+    }
+}

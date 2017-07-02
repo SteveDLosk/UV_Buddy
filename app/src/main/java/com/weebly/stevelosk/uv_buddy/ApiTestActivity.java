b1c11994b61e54ac9c026a7a29c961eef382383c
Steve Losk <Stevedlosk@gmail.com>
2017-07-01 22:45:39 -0700

    ApiTest now gets time and passes in to Async method

--- app/src/main/java/com/weebly/stevelosk/uv_buddy/ApiTestActivity.java
+++ app/src/main/java/com/weebly/stevelosk/uv_buddy/ApiTestActivity.java
@@ -3,10 +3,12 @@ package com.weebly.stevelosk.uv_buddy;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.Calendar;
 
 public class ApiTestActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class ApiTestActivity extends AppCompatActivity {
     private EditText testZipCodeEditText;
     private Button testGetUV_IndexButton;
     private Button testUseLocationServiceButton;
+    private final String TAG = "ApiTestActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +41,7 @@ public class ApiTestActivity extends AppCompatActivity {
                 // pass in current zipcode and hour
                 String zipCode = testZipCodeEditText.getText().toString();
                 // TODO: get current hour
-                String hour = "05 PM";
+                String hour = getTime();
                 GetUV_IndexAsync task = new GetUV_IndexAsync(
                         callingActivity, zipCode, hour);
                 task.execute();
@@ -50,6 +53,56 @@ public class ApiTestActivity extends AppCompatActivity {
     }
 
     protected void update(Integer index) {
-        testResultTextView.setText(index.toString());
+        try {
+            testResultTextView.setText(index.toString());
+        }
+        catch (NullPointerException e) {
+            testResultTextView.setText("Something went wrong");
+        }
+    }
+
+    private String getTime () {
+        Boolean PM = false;
+
+        Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+
+        Log.i(TAG, Integer.toString(hour));
+
+        // Format for AM / PM
+        if (hour >= 12) {
+            PM = true;
+        }
+        if (hour > 12) {
+            hour -= 12;
+        }
+
+        // fix 12 AM
+        if (hour == 0) {
+            hour = 12;
+        }
+
+        // fix no JSON for before 4AM
+        if (hour < 4 && !PM) {
+            hour = 4;
+        }
+
+        // Turn to String
+        String hourString = Integer.toString(hour);
+        if (hour < 10) {
+            hourString = "0" + hourString;
+        }
+
+        // formatting
+        hourString += " ";
+        if (PM) {
+            hourString += "PM";
+        }
+        else {
+            hourString += "AM";
+        }
+
+        Log.i(TAG, hourString);
+        return hourString;
     }
 }

fda366f6ded184bf42fdc60f5d3904d6fc74336c
Steve Losk <Stevedlosk@gmail.com>
2017-07-07 20:18:50 -0700

    Implemented http connection and data pull.  Need to test.

--- app/src/main/java/com/weebly/stevelosk/uv_buddy/GetUV_IndexAsync2.java
+++ app/src/main/java/com/weebly/stevelosk/uv_buddy/GetUV_IndexAsync2.java
@@ -4,6 +4,14 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by steve on 7/7/2017.
  */
@@ -35,8 +43,58 @@ public class GetUV_IndexAsync2 extends AsyncTask <String, Void, Integer[]> {
         // flag for errors
         Boolean errors = false;
 
-        String json = "";
-        UV_Json_Parse uv_json_parse = new UV_Json_Parse(json);
+        // objects to handle data collection
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        String baseUriString = "https://www.wunderground.com/weather/api/d/d4b92a82e8213ee0/edit.html?api_action=changesubscription&api_history=undefined&api_usage=0&api_package=a";
+        String jsonReturned = "";
+
+        // get the json
+        try {
+            URL url = new URL(baseUriString);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+
+            InputStream stream = connection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+                Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+            }
+
+            jsonReturned = buffer.toString();
+            Log.i(TAG, "Here is the JSON:");
+            Log.i(TAG, jsonReturned);
+
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (StringIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        UV_Json_Parse uv_json_parse = new UV_Json_Parse(jsonReturned);
         return uv_json_parse.getDataArray();
     }
 

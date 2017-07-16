b9915c18b46d804c28bbee8fe11428a8bf95e057
Steve Losk <Stevedlosk@gmail.com>
2017-07-16 12:40:33 -0700

    Added sun icon to images and toolbar

---
+++ app/src/main/java/com/weebly/stevelosk/uv_buddy/DisplayEpaScale.java
@@ -0,0 +1,15 @@
+package com.weebly.stevelosk.uv_buddy;
+
+import android.content.res.Resources;
+import android.net.Uri;
+
+/**
+ * Created by steve on 7/16/2017.
+ */
+
+public class DisplayEpaScale {
+
+    //private Resources res = getApplicationContext().getResources();
+    private Uri epaScaleUri;
+    //private String epaScaleString = R.
+}

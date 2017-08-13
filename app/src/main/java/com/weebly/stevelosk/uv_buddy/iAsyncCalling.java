6f699055d437c8482f1373fadd2f85fd7bdbe8af
Steve Losk <Stevedlosk@gmail.com>
2017-08-12 20:21:47 -0700

    tracking images

---
+++ app/src/main/java/com/weebly/stevelosk/uv_buddy/iAsyncCalling.java
@@ -0,0 +1,12 @@
+package com.weebly.stevelosk.uv_buddy;
+
+/**
+ * Created by steve on 7/16/2017.
+ */
+
+public interface iAsyncCalling {
+
+    public void update(Integer result);
+
+    public void reportLoading();
+}

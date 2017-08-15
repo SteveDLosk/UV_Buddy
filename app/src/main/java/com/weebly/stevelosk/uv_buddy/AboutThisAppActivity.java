10567150b7d703b01f15f37772265e31d31fc326
Steve Losk <Stevedlosk@gmail.com>
2017-08-14 21:26:21 -0700

    Wunderground logo in about navigates to the site

--- app/src/main/java/com/weebly/stevelosk/uv_buddy/AboutThisAppActivity.java
+++ app/src/main/java/com/weebly/stevelosk/uv_buddy/AboutThisAppActivity.java
@@ -1,13 +1,31 @@
 package com.weebly.stevelosk.uv_buddy;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutThisAppActivity extends AppCompatActivity {
+
+    private ImageView wunderGroundImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_this_app);
+
+        wunderGroundImg = (ImageView) findViewById(R.id.wundergroundImg);
+        wunderGroundImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // navigate to Wunderground, using referral
+                Uri referralUri = Uri.parse("https://www.wunderground.com/?apiref=883a4071c708e116");
+                Intent navToWundergroundIntent = new Intent(Intent.ACTION_VIEW);
+                navToWundergroundIntent.setData(referralUri);
+                startActivity(navToWundergroundIntent);
+            }
+        });
     }
 }
